ece473e8e5e3a0e51df454d91b8e4370b4cb370b
aliaksandraleanovich <a.lenovich@softteco.com>
2023-04-26 14:10:47 +0300

    feat: module app and methods

--- electron/src/main/index.js
+++ electron/src/main/index.js
@@ -1,56 +1,39 @@
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
-import path from 'path'
-import { app, BrowserWindow } from 'electron'
+import { app } from "electron";
 
-const createWindow = () => {
-  // Create the browser window.
-  let win = new BrowserWindow({
-    title: CONFIG.name,
-    width: CONFIG.width,
-    height: CONFIG.height,
-    webPreferences: {
-      worldSafeExecuteJavaScript: true,
-      preload: path.join(app.getAppPath(), 'preload', 'index.js')
+app.on("will-finish-launching", () => {
+  console.log("will finish launching");
+});
+
+app.on("ready", () => {
+  console.log("ready");
+});
+
+app.whenReady().then(() => console.log("app is ready"));
+
+app.on("before-quit", () => console.log("before quit"));
+app.on("will-quit", () => console.log("will quit")); // порядок отображния за счет методов жизненных циклов
+app.on("quit", () => console.log("quit"));
+
+app.whenReady().then(() => {
+  setTimeout(() => {
+    app.quit();
+  }, 2000);
+});
+
+//запрет на вызов второго экземпляра приложения
+const lock = app.requestSingleInstanceLock();
+
+if (!lock) {
+  app.quit();
+} else {
+  app.on("second-instance", () => {
+    console.log("second");
+    if (win) {
+      win.focus();
     }
-  })
-
-  // and load the index.html of the app.
-  win.loadFile('renderer/index.html')
-
-  // send data to renderer process
-  win.webContents.on('did-finish-load', () => {
-    win.webContents.send('loaded', {
-      appName: CONFIG.name,
-      electronVersion: process.versions.electron,
-      nodeVersion: process.versions.node,
-      chromiumVersion: process.versions.chrome
-    })
-  })
-
-  win.on('closed', () => {
-    win = null
-  })
+  });
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow)
-
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
-})
+app.whenReady().then(() => {
+  app.showAboutPanel();
+});
